533ac7442651cd78cfcf813d3db87748248ee229
suneetchugh-dev <valorise078gramming@gmail.com>
2026-05-21 21:32:24 +0530

    Changed logo import from PNG to WebP for better performance

--- CodeBase/client/src/App.jsx
+++ CodeBase/client/src/App.jsx
@@ -12,7 +12,7 @@ import NotFound from './pages/NotFound';
 import './App.css';
 import { useEffect, useState, useRef } from 'react';
 import { darkTheme, lightTheme } from './theme';
-import MainLogo from './assets/main-logo.png';
+import MainLogo from './assets/main-logo.webp';
 
 
 function App() {
