1cfd48477bddefc62d7435514c1e28d9af878852
suneetchugh-dev <valorise078gramming@gmail.com>
2026-05-22 13:12:22 +0530

    Fix mic icon alignment and add hover animation to Join Voice Room button

--- CodeBase/client/src/pages/JoinRoom.jsx
+++ CodeBase/client/src/pages/JoinRoom.jsx
@@ -118,7 +118,7 @@ function JoinRoom() {
         onClick={handleJoin}
         disabled={isFull}
       >
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/></svg>
+        <svg className="join-room-mic-icon" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/></svg>
         {isFull ? 'Room is Full' : 'Join Voice Room'}
       </button>
     </section>
